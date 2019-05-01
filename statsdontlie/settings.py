c43022b7fc262232b1b120a35e6a919a2786445f
Purab Shah <purabshah8@gmail.com>
2019-05-01 13:36:40 -0400

    remove whitenoise from staticfiles storage

--- statsdontlie/settings.py
+++ statsdontlie/settings.py
@@ -143,7 +143,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'assets'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Webpack Loader
 WEBPACK_LOADER = {
